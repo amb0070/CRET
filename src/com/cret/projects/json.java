130fa2d5f12dfeee1e8d5d1d7acedc0d28f7f3a1
Adrian Marcos <37064592+amb0070@users.noreply.github.com>
2018-12-24 23:44:49 +0100

    Json and project manager classes
    Initial json and project management classes.

---
+++ src/com/cret/projects/json.java
@@ -0,0 +1,14 @@
+package com.cret.projects;
+
+public class json {
+	
+	
+	public void exportJson() {
+		
+	}
+	
+	public void importJson() {
+		
+	}
+
+}
---
+++ src/com/cret/projects/projectManager.java
@@ -0,0 +1,21 @@
+package com.cret.projects;
+
+public class projectManager {
+
+	
+	public void openProject() {
+		
+	}
+	
+	
+	public void saveProject() {
+		
+	}
+	
+	
+	public void getListOfProjects() {
+		
+	}
+	
+	
+}
