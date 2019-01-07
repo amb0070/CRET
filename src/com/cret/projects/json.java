eac6bd1776f43cbcb845c7460793e2490758cfaf
Adrian Marcos <37064592+amb0070@users.noreply.github.com>
2019-01-07 09:32:16 +0100

    Project classes
    Updated project manager classes

--- src/com/cret/projects/json.java
+++ src/com/cret/projects/json.java
@@ -1,29 +1,81 @@
 package com.cret.projects;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.List;
 import java.util.Map;
+import org.json.*;
 
 public class json {
 	
+	public static String fileToString(File file) throws IOException {
+		
+		BufferedReader reader = new BufferedReader(new FileReader(file));
+		StringBuilder stringBuilder = new StringBuilder();
+		String line = null;
+		String ls = System.getProperty("line.separator");
+		while ((line = reader.readLine()) != null) {
+			stringBuilder.append(line);
+			stringBuilder.append(ls);
+		}
+		// delete the last new line separator
+		stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+		reader.close();
+
+		String content = stringBuilder.toString();
+		
+		return content;
+	}
+
 	
-	public void exportJSONProject(String name, Map<String, List<String>> values) {
+	public static boolean isTextFile(File f) throws IOException {
+        String type = Files.probeContentType(f.toPath());
+        if (type == null) {
+            //type couldn't be determined, assume binary
+            return false;
+        } else if (type.startsWith("text")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+	
+	public static boolean isJSON(String test) {
+		
+	    try {
+	    	
+	        new JSONObject(test);
+	        
+	    } catch (JSONException ex) {
+	        try {
+	            new JSONArray(test);
+	        } catch (JSONException ex1) {
+	            return false;
+	        }
+	    }
+	    return true;
+	}
+	
+	
+	public static void exportJSONProject(String name, Map<String, List<String>> values) {
 		
 		//Get data, generate json and open window dialog to store it.
 		
 	}
 	
-	public void exportJSONProjects() {
+	public static void exportJSONProjects(String db) {
 		//Export all database
 	}
 	
-	public void importJSONProject() {
+	public static void importJSONProjects(String file) {
 		
 		//Open window, select file, open, parse it and store in project options.
-		
-		
+
 		
 	}
 	
-	
-
 }
--- src/com/cret/projects/project.java
+++ src/com/cret/projects/project.java
@@ -1,12 +1,17 @@
 package com.cret.projects;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class project {
+import com.cret.gui.GuiUtils;
+
+import javafx.scene.control.Alert.AlertType;
+
+public class Project {
 	
-	public project(String name) {
+	public Project(String name) {
 		
 		setProjectName(name);
 		
@@ -25,47 +30,112 @@ public class project {
 	//Map with analyzed can data
 	//Structure:
 		//Data, ID, Byte
+			//List 0 - ID
+			//List 1 - Byte
 	
 	private Map<String, List<String>> analyzedValues = new HashMap<>();
 	
 	
+	//DONE
 	public void setProjectName(String name) {
 		projectName = name;
 	}
 	
+	//DONE
 	public String getProjetName() {
 		return this.projectName;
 	}
 	
+	//DONE
 	public Map<String, List<String>> getAllData(){
 		return analyzedValues;
 	}
 	
+	//TO TEST
 	//Return map value 1 from key (ID)
 	public String getMapValue1(String key) {
-		return analyzedValues.get
+		
+		if (analyzedValues.containsKey(key)) {
+			
+			List<String> values = analyzedValues.get(key);
+			
+			return values.get(0);
+			
+		} else {
+			return "";
+		}
 	}
 	
+	//TO TEST
 	//Return map value 2 from key (byte)
 	public String getMapValue2(String key) {
-		return "";
+		
+		if (analyzedValues.containsKey(key)) {
+			
+			List<String> values = analyzedValues.get(key);
+			
+			return values.get(1);
+			
+		} else {
+			return "";
+		}
 	}
 	
+	//TO TEST
 	//Return all keys (identified names)
-	public String [] getMapKeys() {
-		return new String[]{""};
+	public List<String> getMapKeys() {
+		
+		List<String> list = new ArrayList<String>(analyzedValues.keySet());
+		
+		return list;
 	}
 	
+	//TO TEST
 	public void setCompleteMap(Map<String, List<String>> newMap) {
 		analyzedValues = newMap;
 	}
 	
 	
+	//TO TEST
 	public void setMapKey(String key) {
+		
+		if (analyzedValues.containsKey(key)) {
+			GuiUtils.generateAlert(AlertType.ERROR, "ERROR EN MAP", "key repetidas bro");
+		} else {
+			
+			List<String> list = new ArrayList<String>();
+			//Add empty list
+			analyzedValues.put(key, list);
+		}
+
 		
 	}
 	
+	//TO TEST
 	public void setMapValues(String key, String id, String byteData) {
+		
+		if (analyzedValues.containsKey(key)) {
+			
+			List<String> list = new ArrayList<String>();
+			list.add(id);
+			list.add(byteData);
+			
+			analyzedValues.put(key, list);
+			
+		} else {
+			
+			System.out.println("No key on map");
+		}
+		
+	}
+	
+	
+	//Save project in database
+	public void saveProject() {
+		
+		String sqlProject = "INSERT INTO projects VALUES(?)";
+		String sqlData = "INSERT INTO data VALUES (?,?,?,?)";
+		
 		
 	}
 
