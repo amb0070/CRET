32a21834b331351782f4149c8d7a17bbc05e269d
Adrian Marcos <37064592+amb0070@users.noreply.github.com>
2019-01-11 00:04:11 +0100

    Update of new project window
    Update of the controller and the FXML document of new project window.

--- src/com/cret/gui/NewProjectController.java
+++ src/com/cret/gui/NewProjectController.java
@@ -1,13 +1,19 @@
 package com.cret.gui;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import com.cret.gui.GuiUtils;
 
 public class NewProjectController {
 
+	public String newProjectName = "";
 	
 	@FXML
 	private Button btnClose;
@@ -18,10 +24,31 @@ public class NewProjectController {
 	@FXML
 	private TextField txtProjectName;
 	
+	
+	//DONE
 	@FXML
 	private void closeWindow(ActionEvent event) throws Exception{
-	    Stage stage = (Stage) btnClose.getScene().getWindow();
-	    stage.close();
+		Platform.exit();
+	}
+	
+	//DONE
+	@FXML
+	private void createProject(ActionEvent event) throws Exception{
+		
+		String projectName = txtProjectName.getText().trim();
+		
+		
+		//Empty project
+		if (projectName.equals("")) {
+			
+			GuiUtils.generateAlert(AlertType.INFORMATION, "Invalid project name", "An empty text is not a valid project name");
+		} else {
+			
+			RootViewController.returnNewName(projectName);
+			Stage stage = (Stage) btnClose.getScene().getWindow();
+            stage.close();
+		}
+		
 	}
 	
 	
