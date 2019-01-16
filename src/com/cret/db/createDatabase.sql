d3aa981352a80363d0b44d7d91811b0ed320c9a2
Adrian Marcos <37064592+amb0070@users.noreply.github.com>
2019-01-16 10:40:22 +0100

    Update createDatabase.sql

--- src/com/cret/db/createDatabase.sql
+++ src/com/cret/db/createDatabase.sql
@@ -6,7 +6,7 @@ CREATE TABLE projects (
 CREATE TABLE data (
 	name varchar(20) NOT NULL,
 	ID varchar(10) NOT NULL,
-	byte varchar(2) NOT NULL,
+	byte varchar(20) NOT NULL,
 	projectName varchar(30) NOT NULL,
 	PRIMARY KEY (name, ID, byte, projectName),
 	FOREIGN KEY (projectName) REFERENCES projects(name)
