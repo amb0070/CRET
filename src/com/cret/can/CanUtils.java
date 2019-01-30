41afe533755e6f531d5c7bed7ad7de487cc6c847
Adrian Marcos <37064592+amb0070@users.noreply.github.com>
2019-01-30 20:15:51 +0100

    Update CanUtils.java
    Commented and removed unnecessary code.

--- src/com/cret/can/CanUtils.java
+++ src/com/cret/can/CanUtils.java
@@ -3,73 +3,200 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-
 import jssc.SerialPortList;
 
-
 /**
+ * 
+ * Class with static methods. Utils for CAN interaction.
+ * 
+ * Used to know which interface is configurred, and the configuration characteristics of the interface.
+ * 
+ * Methods to set and get the configuration of the interfaces and filters.
+ * 
  * @author Adrian Marcos
+ * @version 1.0
  *
  */
 public class CanUtils {
 	
+	/**
+	 * Pair with ID - List<Byte number> for interface 1.
+	 */
 	private static Map<String, List<Integer>> identifiedIdsInterface1 = new HashMap<>();
 	
+	/**
+	 * Pair with ID - Length of the frame for interface1.
+	 */
 	private static Map<String, Integer> identifiedIdsInterface1Len = new HashMap<>();
 	
+	/**
+	 * Pair with ID - List<Byte number> for interface 2.
+	 */
 	private static Map<String, List<Integer>> identifiedIdsInterface2 = new HashMap<>();
 	
+	/**
+	 * Pair with ID - Length of the frame for interface 2.
+	 */
 	private static Map<String, Integer> identifiedIdsInterface2Len = new HashMap<>();
 	
+	/**
+	 * Selected speed for interface 1.
+	 */
 	private static String speedInterface1;
 	
+	/**
+	 * Selected speed for interface 2.
+	 */
 	private static String speedInterface2;
 	
+	/**
+	 * Selected port for interface 1.
+	 */
 	private static String portInterface1;
 	
+	/**
+	 * Selected port for interface 2.
+	 */
 	private static String portInterface2;
 	
+	/**
+	 * Selected mode for interface 1.
+	 */
 	private static String modeInterface1;
 	
+	/**
+	 * Selected mode for interface 2.
+	 */
 	private static String modeInterface2;
 	
+	/**
+	 * Stores if interface 1 is enabled and configured.
+	 */
 	private static boolean isInterface1Enabled = false;
 	
+	/**
+	 * Stores if interface 2 is enabled and configured.
+	 */
 	private static boolean isInterface2Enabled = false;
+
+	/**
+	 * Ignore if bytes to plot on interface 1 are 00.
+	 */
+	private static boolean ignoreZeroBytesInterface1 = true;
 	
-	private static boolean ignoreZeroBytes = true;
+	/**
+	 * Ignore if bytes to plot on interface 2 are 00.
+	 */
+	private static boolean ignoreZeroBytesInterface2 = true;
 	
-	private static boolean splitBytes = false;
+	/**
+	 * Split bytes into nibbles on interface 1.
+	 * For example, A0 -> A - 0
+	 */
+	private static boolean splitBytesInterface1 = false;
+	
+	/**
+	 * Split bytes into nibbles on interface 2.
+	 * For example, A0 -> A - 0
+	 */
+	private static boolean splitBytesInterface2 = false;
 	
 	
-	public static void clearInterface1() {
+	/**
+	 * Clears the identified IDs and bytes of the interface 1.
+	 */
+	public static void clearIdentifiedInterface1() {
 		identifiedIdsInterface1 = new HashMap<>();
 		identifiedIdsInterface1Len = new HashMap<>();
 	}
 	
-	public static void clearInterface2() {
+	/**
+	 * Clears the identified IDs and bytes of the interface 2.
+	 */
+	public static void clearIdentifiedInterface2() {
 		identifiedIdsInterface2 = new HashMap<>();
 		identifiedIdsInterface2Len = new HashMap<>();
 	}
 	
-	public static void setIgnoreZeroBytes(boolean option) {
-		ignoreZeroBytes = option;
+	/**
+	 * Sets the boolean option to ignore bytes with value 00 for interface 1.
+	 * 
+	 * @param option Boolean option to ignore bytes with value 00.
+	 */
+	public static void setIgnoreZeroBytesInterface1(boolean option) {
+		ignoreZeroBytesInterface1 = option;
 	}
 	
-	public static boolean getIgnoreZeroBytes() {
-		return ignoreZeroBytes;
+	/**
+	 * Sets the boolean option to ignore bytes with value 00 for interface 2.
+	 * @param option True to ignore bytes with value 00.
+	 */
+	public static void setIgnoreZeroBytesInterface2(boolean option) {
+		ignoreZeroBytesInterface2 = option;
 	}
 	
-	public static void setSplitBytes(boolean option) {
-		splitBytes = option;
+	/**
+	 * Returns if interface 1 is ignoring bytes with value 00.
+	 * 
+	 * @return True if interface 1 ignores 00 values.
+	 */
+	public static boolean getIgnoreZeroBytesInterface1() {
+		return ignoreZeroBytesInterface1;
 	}
 	
-	public static boolean getSplitBytes() {
-		return splitBytes;
+	/**
+	 * Returns if interface 2 is ignoring bytes with value 00.
+	 * 
+	 * @return True if interface 2 ignores 00 values.
+	 */
+	public static boolean getIgnoreZeroBytesInterface2() {
+		return ignoreZeroBytesInterface2;
+	}
+	
+	/**
+	 * Sets if interface 1 has to ignore bytes with value 00.
+	 * 
+	 * @param option True if splits bytes.
+	 */
+	public static void setSplitBytesInterface1(boolean option) {
+		splitBytesInterface1 = option;
+	}
+	
+	/**
+	 * Sets if interface 2 has to ignore bytes with value 00.
+	 * 
+	 * @param option True if splits bytes.
+	 */
+	public static void setSplitBytesInterface2(boolean option) {
+		splitBytesInterface2 = option;
+	}
+	
+	/**
+	 * Returns if interface 1 splits bytes in two values.
+	 * 
+	 * @return True if splits bytes.
+	 */
+	public static boolean getSplitBytesInterface1() {
+		return splitBytesInterface1;
+	}
+	
+	/**
+	 * Returns if interface 2 splits bytes in two values.
+	 * 
+	 * @return True if splits bytes.
+	 */
+	public static boolean getSplitBytesInterface2() {
+		return splitBytesInterface2;
 	}
 	
 	
+	/**
+	 * Returns true if the pair ID - byte number is identified by the interface 1.
+	 * 
+	 * @param id Identifier of the identified frame.
+	 * @param byteNumber Byte position in the frame.
+	 * @return True if the pair is identified.
+	 */
 	public static boolean isIdentifiedInterface1(String id, int byteNumber) {
 		if (identifiedIdsInterface1.containsKey(id)) {
 			if (identifiedIdsInterface1.get(id).contains(byteNumber)){
@@ -82,6 +209,13 @@ public class CanUtils {
 		}
 	}
 	
+	
+	/**
+	 * Returns true if ID is identified by the interface 1.
+	 * 
+	 * @param id ID of the frame.
+	 * @return True if it's identified.
+	 */
 	public static boolean isIdentifiedInterface1Len(String id) {
 		if (identifiedIdsInterface1Len.containsKey(id)) {
 			return true;
@@ -90,14 +224,66 @@ public class CanUtils {
 		}
 	}
 	
-	public static boolean isIdentifiedInterface2(String id) {
-		if (identifiedIdsInterface1Len.containsKey(id)) {
-			return true;
+
+	/**
+	 * Returns if the pair ID - byte number is identified by interface 2.
+	 * 
+	 * @param id ID of the frame.
+	 * @param byteNumber Byte position in the frame.
+	 * @return True if it's identified.
+	 */
+	public static boolean isIdentifiedInterface2(String id, int byteNumber) {
+		if (identifiedIdsInterface2.containsKey(id)) {
+			if (identifiedIdsInterface2.get(id).contains(byteNumber)){
+				return true;
+			} else {
+				return false;
+			}
 		} else {
 			return false;
 		}
 	}
 	
+	/**
+	 * Returns if the ID is identified by interface 2.
+	 * 
+	 * @param id ID of the frame.
+	 * @return True if it's identified.
+	 */
+	public static boolean isIdentifiedInterface2Len(String id) {
+		if (identifiedIdsInterface2Len.containsKey(id)) {
+			return true;
+		} else {
+			return false;
+		}
+ 	}
+	
+	/**
+	 * Sets the pair ID - byte number in the interface 1.
+	 * 
+	 * @param id ID of the frame.
+	 * @param byteNumber Byte position in the frame.
+	 */
+	public static void setIdentifiedIdInterface1(String id, int byteNumber) {
+		identifiedIdsInterface1.get(id).add(byteNumber);
+	}
+	
+	/**
+	 * Sets the pair ID - byte number in interface 2.
+	 * 
+	 * @param id ID of the frame.
+	 * @param byteNumber Byte position in the frame.
+	 */
+	public static void setIdentifiedIdInterface2(String id, int byteNumber) {
+		identifiedIdsInterface2.get(id).add(byteNumber);
+	}
+	
+	/**
+	 * Sets the pair ID - length of a frame in the interface 1.
+	 * 
+	 * @param id ID of the frame.
+	 * @param length Lenght of the data field.
+	 */
 	public static void setIdentifiedIdInterface1Len(String id, int length) {
 		identifiedIdsInterface1Len.put(id, length);
 		for (int i=0; i < length; i++) {
@@ -105,114 +291,263 @@ public class CanUtils {
 		}
 	}
 	
-	public static void setIdentifiedIdInterface1(String id, int byteNumber) {
-
-		identifiedIdsInterface1.get(id).add(byteNumber);
-	}
-	
+	/**
+	 * Sets the pair ID - length of a frame in the interface 2.
+	 * 
+	 * @param id ID of the frame.
+	 * @param length Length of the data field.
+	 */
 	public static void setIdentifiedIdInterface2Len(String id, int length) {
 		identifiedIdsInterface2Len.put(id, length);
 	}
 	
-	public static List<String> getListIdsInterface1Len(){
+	/**
+	 * Returns a list of the IDs identified by interface 1.
+	 * 
+	 * @return List with the identified IDs.
+	 */
+	public static List<String> getIdsIdentifiedInterface1Len(){
 		List<String> ids = new ArrayList<String>();
 		ids.addAll(identifiedIdsInterface1Len.keySet());
 		return ids;
 	}
 	
-	public static List<String> getListIdsInterface2Len(){
+	/**
+	 * Returns a list of the IDs identified by interface 2.
+	 * 
+	 * @return List with the identified IDs.
+	 */
+	public static List<String> getIdsIdentifiedInterface2Len(){
 		List<String> ids = new ArrayList<String>();
 		ids.addAll(identifiedIdsInterface2Len.keySet());
 		return ids;
 	}
 	
-	public static int getLengthIdInterface1Len(String id) {
+	/**
+	 * Returns the length of the frame of a specified ID identified by interface 2.
+	 * 
+	 * @param id ID of the frame.
+	 * @return Length of the identified ID.
+	 */
+	public static int getLengthOfIdInterface1Len(String id) {
 		return identifiedIdsInterface1Len.get(id);
 	}
 	
-	public static int getLengthIdInterface2Len(String id) {
+	/**
+	 * Returns the length of the frame of a specified ID identified by interface 2.
+	 * 
+	 * @param id ID of the frame.
+	 * @return Length of the identified ID.
+	 */
+	public static int getLengthOfIdInterface2Len(String id) {
 		return identifiedIdsInterface2Len.get(id);
 	}
 	
-	public static void setConfigSpeed1(String speed) {
+	/**
+	 * Sets the speed to be used by interface 1.
+	 * 
+	 * @param speed Speed to connect.
+	 */
+	public static void setConfigurationSpeedInterface1(String speed) {
 		speedInterface1 = speed;
 	}
 	
-	public static void setConfigSpeed2(String speed) {
+	/**
+	 * Sets the speed to be used by interface 2.
+	 * 
+	 * @param speed Speed to connect.
+	 */
+	public static void setConfigurationSpeedInterface2(String speed) {
 		speedInterface2 = speed;
 	}
 	
-	public static void setConfigPort1(String port) {
+	/**
+	 * Sets the port to be used by interface 1.
+	 * 
+	 * @param port Port to connect.
+	 */
+	public static void setConfigurationPortInterface1(String port) {
 		portInterface1 = port;
 	}
 	
-	public static void setConfigPort2(String port) {
+	/**
+	 * Sets the port to be used by interface 2.
+	 * 
+	 * @param port Port to connect.
+	 */
+	public static void setConfigurationPortInterface2(String port) {
 		portInterface2 = port;
 	}
 	
-	public static void set1(boolean state) {
+	/**
+	 * Sets if interface 1 is configured and enabled.
+	 * 
+	 * @param state State of the interface.
+	 */
+	public static void setInterface1State(boolean state) {
 		isInterface1Enabled = state;
 	}
 	
-	public static void set2(boolean state) {
+	/**
+	 * Sets if interface 2 is configured and enabled.
+	 * 
+	 * @param state State of the interface.
+	 */
+	public static void setInterface2State(boolean state) {
 		isInterface2Enabled = state;
 	}
 	
-	public static String getConfigSpeed1() {
+	/**
+	 * Returns the speed configured to interface 1.
+	 * 
+	 * @return Speed of the interface 1.
+	 */
+	public static String getConfigurationSpeedInterface1() {
 		return speedInterface1;
 	}
 	
-	public static String getConfigSpeed2() {
+	/**
+	 * Returns the speed configured for interface 2.
+	 * 
+	 * @return Speed of the interface 2.
+	 */
+	public static String getConfigurationSpeedInterface2() {
 		return speedInterface2;
 	}
 	
-	public static String getConfigPort1() {
+	/**
+	 * Returns the port configured for interface 1.
+	 * 
+	 * @return Port of the interface 1.
+	 */
+	public static String getConfigurationPortInterface1() {
 		return portInterface1;
 	}
 	
-	public static String getConfigPort2() {
+	/**
+	 * Returns the port configured for interface 2.
+	 * 
+	 * @return Port of the interface 2.
+	 */
+	public static String getConfigurationPortInterface2() {
 		return portInterface2;
 	}
 	
-	public static boolean get1State() {
+	/**
+	 * Returns if interface 1 is enabled and configured.
+	 * 
+	 * @return True if interface is enabled.
+	 */
+	public static boolean getInterface1State() {
 		return isInterface1Enabled;
 	}
 	
-	public static boolean get2State() {
+	/**
+	 * Returns if interface 2 is enabled and configured.
+	 * 
+	 * @return True if interface is enabled.
+	 */
+	public static boolean getInterface2State() {
 		return isInterface2Enabled;
 	}
 	
-	public static String getConfigMode1() {
+	/**
+	 * Returns the mode configured to interface 1.
+	 * 
+	 * @return Mode configured on interface 1.
+	 */
+	public static String getConfigurationModeInterface1() {
 		return modeInterface1;
 	}
 	
-	public static String getConfigMode2() {
+	/**
+	 * Returns the mode configured to interface 2.
+	 * 
+	 * @return Mode configured on interface 2.
+	 */
+	public static String getConfigurationModeInterface2() {
 		return modeInterface2;
 	}
 	
-	public static void setConfigMode1(String mode) {
+	/**
+	 * Sets the mode that interface 1 will use to connect to the port.
+	 * 
+	 * @param mode Mode to configure interface 1.
+	 */
+	public static void setConfigurationModeInterface1(String mode) {
 		modeInterface1 = mode;
 	}
 	
-	public static void setConfigMode2(String mode) {
+	/**
+	 * Sets the mode that interface 2 will use to connect to the port.
+	 * 
+	 * @param mode Mode to configure interface 2.
+	 */
+	public static void setConfigurationModeInterface2(String mode) {
 		modeInterface2 = mode;
 	}
 	
+	
+	/**
+	 * Returns decimal value of hexadecimal string input.
+	 * 
+	 * @param hex Hex value.
+	 * @return Decimal value.
+	 */
 	public static int hexToDec(String hex) {
 		return Integer.parseInt(hex, 16);
 	}
 	
+	/**
+	 * Returns hexadecimal String of decimal input.
+	 * 
+	 * @param dec Decimal value.
+	 * @return Hexadecimal value.
+	 */
 	public static String decToHex(int dec) {
 		return Integer.toHexString(dec);
 	}
 	
+	/**
+	 * Returns decimal value of hexadecimal byte input.
+	 * 
+	 * @param hex Hexadecimal value.
+	 * @return Decimal value.
+	 */
 	public static int hexToDec(Byte hex) {
 		int value = 0;
 		value = (value << 8) | hex;
 		return value;
 	}
 	
-	//Returns the COM ports
+	/**
+	 * Turns hexadecimal value to ascii value.
+	 * 
+	 * @param hex Hexadecimal value.
+	 * @return String value.
+	 */
+	public static String convertHexToAscii(String hex){
+
+		  StringBuilder sb = new StringBuilder();
+
+		  for( int i=0; i<hex.length()-1; i+=2 ){
+			  
+		      String output = hex.substring(i, (i + 2));
+		      
+		      int decimal = Integer.parseInt(output, 16);
+
+		      sb.append((char)decimal);
+		  }
+		  
+		  return sb.toString();
+	  }
+	
+	
+	/**
+	 * Returns the list of COM ports available.
+	 * 
+	 * @return Array with COM ports.
+	 */
 	public static String[] getSerialPorts() {
 		
 		String[] ports = SerialPortList.getPortNames();
@@ -223,6 +558,5 @@ public class CanUtils {
 		} else {
 			return ports;
 		}
-		
 	}
 }
