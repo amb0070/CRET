1ddcc7e8cbb0c9e4cf767a01a5ed958a0b056282
Adrian Marcos <37064592+amb0070@users.noreply.github.com>
2019-01-30 18:59:07 +0100

    Update CanConnection.java
    Commented and removed unnecesary code.

--- src/com/cret/can/CanConnection.java
+++ src/com/cret/can/CanConnection.java
@@ -1,51 +1,76 @@
 package com.cret.can;
 
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Random;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.function.Consumer;
-
 import com.cret.gui.GuiUtils;
 import com.cret.gui.RootViewController;
 import com.cret.staticData.structures;
-
-import de.fischl.usbtin.CANMessage;
-import de.fischl.usbtin.CANMessageListener;
 import de.fischl.usbtin.USBtin;
 import de.fischl.usbtin.USBtin.OpenMode;
-import javafx.application.Platform;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.concurrent.Task;
-import javafx.scene.chart.XYChart;
 import javafx.scene.control.Alert.AlertType;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
 import de.fischl.usbtin.USBtinException;
 
-
+/**
+ * CanConnection.java - Connection module to CAN Bus network.
+ * 
+ * Object to connect to CAN network. 3 possible listeners to use.
+ * 
+ * @author Adrian Marcos
+ * @version 1.0
+ *
+ */
 public class CanConnection{
 
+	/**
+	 * Port to connect.
+	 */
 	private String port;
+	
+	/**
+	 * Speed of the connection.
+	 */
 	private int speed;
+	
+	/**
+	 * Connection mode. (ACTIVE, LOOPBACK and LISTENONLY)
+	 */
 	private OpenMode mode;
 	
+	/**
+	 * Object to connect to CAN network.
+	 */
 	protected USBtin connection;
 	
+	/**
+	 * Listener for analysis connection.
+	 */
 	private CanListenerAnalysis listenerAnalysis;
 	
+	/**
+	 * Listener for dashboard connection.
+	 */
 	private CanListenerDashboard listenerDashboard;
 	
+	/**
+	 * Listener for RAW connection.
+	 */
 	private CanListenerRAW listenerRAW;
 	
-	RootViewController controller;
-
+	/**
+	 * Main controller. Used to set elements from another thread.
+	 */
+	private RootViewController controller;
 	
 	
-	public CanConnection(String port, String speed, String mode, RootViewController controller) throws IOException, USBtinException {
+	/**
+	 * 
+	 * Constructor. Sets the port, speed and mode to connect to CAN network.
+	 * 
+	 * @param port Port to connect.
+	 * @param speed Speed of the connection.
+	 * @param mode Mode of the connection. (ACTIVE, LISTEN-ONLY or LOOPBACK).
+	 * @param controller Main FXML controller. Used to modify the GUI from this class.
+	 * @throws USBtinException Exception if there is any error connecting to the port.
+	 */
+	public CanConnection(String port, String speed, String mode, RootViewController controller) throws USBtinException {
 		
 		
 		this.controller = controller;
@@ -55,44 +80,60 @@ public class CanConnection{
 		this.speed = Integer.parseInt(speed);
 		
 		if (mode.equals("ACTIVE")) {
+			
 			this.mode = OpenMode.ACTIVE;
+			
 		} else if (mode.equals("LISTENONLY")) {
+			
 			this.mode = OpenMode.LISTENONLY;
+			
 		} else if (mode.equals("LOOPBACK")) {
+			
 			this.mode = OpenMode.LOOPBACK;
+			
 		} else {
+			
+			//Default mode: LISTENONLY.
 			this.mode = OpenMode.LISTENONLY;
+			
 		}
-		
-		
 		
 		connection = new USBtin();
 		
+		/*
+		 * Sets the listener.
+		 */
 		if (structures.mode.equals("Dashboard")) {
+			
 			listenerDashboard = new CanListenerDashboard(controller);
 			connection.addMessageListener(listenerDashboard);
+			
 		} else if (structures.mode.equals("Analysis")) {
+			
 			listenerAnalysis = new CanListenerAnalysis(controller);
 			connection.addMessageListener(listenerAnalysis);
-		} else {
+			
+		} else if (structures.mode.equals("RAW")) {
+			
 			listenerRAW = new CanListenerRAW();
 			connection.addMessageListener(listenerRAW);
+			
+		} else {
+			GuiUtils.generateAlert(AlertType.ERROR, "INTERNAL ERROR", "Selected option is not valid.");
 		}
-
 
 	}
 	
 
-	
-
-	public void connectToPort() {
+	/**
+	 * Connect to to CAN network.
+	 */
+	public void connect() {
+		
 		try {
 			
 			connection.connect(port);
 			connection.openCANChannel(speed, mode);
-			controller.btnStartAnalysis1.setDisable(true);
-			controller.btnStopAnalysis1.setDisable(false);
-			//Connected
 			
 		} catch (USBtinException e) {
 			GuiUtils.generateAlert(AlertType.ERROR, "COM PORT ERROR", "COM Port is busy.");
@@ -100,20 +141,21 @@ public class CanConnection{
 		
 	}
 	
-	
+	/**
+	 * Disconnect from CAN network.
+	 * 
+	 * @throws USBtinException If can't disconnect from port.
+	 */
 	public void disconnect() throws USBtinException {
-		connection.closeCANChannel();
-		connection.disconnect();
-		connection = null;
 		
+		// Close CAN Channel
+		connection.closeCANChannel();
+		
+		//Disconnect from port.
+		connection.disconnect();
+		
+		connection = null;
 		listenerAnalysis = null;
 		listenerDashboard = null;
 	}
-	
-	public void disconnectAll() {
-		
-	}
-
-	
-
 }
