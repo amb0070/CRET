6fb3c92f5910546562269a6b5426e442e38bb0e9
Adrian Marcos <37064592+amb0070@users.noreply.github.com>
2019-01-30 19:31:32 +0100

    Update CanTable.java
    Commented and removed unnecessary code.

--- src/com/cret/can/CanTable.java
+++ src/com/cret/can/CanTable.java
@@ -1,81 +1,162 @@
 package com.cret.can;
 
+import javafx.beans.property.SimpleLongProperty;
 import javafx.beans.property.SimpleStringProperty;
 
+/**
+ * 
+ * Class used to create the necessary structure to show data on a tableView.
+ * 
+ * @author Adrian Marcos
+ * @version 1.0
+ *
+ */
 public class CanTable {
 
-	private SimpleStringProperty time;
+	/**
+	 * Frame time.
+	 */
+	private SimpleLongProperty time;
+	/**
+	 * Identifier of the frame.
+	 */
 	private SimpleStringProperty id;
+	
+	/**
+	 * Length of the data of the frame.
+	 */
 	private SimpleStringProperty length;
+	
+	/**
+	 * Data of the frame.
+	 */
 	private SimpleStringProperty data;
 	
 	/**
+	 * ASCII value of data field.
+	 */
+	private SimpleStringProperty ascii;
+
+	/**
+	 * 
+	 * Constructor. Sets the parameters of a CAN frame.
+	 * 
+	 * @param time Time of the received frame.
+	 * @param id Identifier of the frame.
+	 * @param length Length of data field of the frame.
+	 * @param data Data field.
+	 * @param ascii ASCII value of data field.
 	 * 
 	 */
-	public CanTable(String time, String id, String length, String data) {
+	public CanTable(long time, String id, String length, String data, String ascii) {
 		
-		this.time = new SimpleStringProperty(time);
+		this.time = new SimpleLongProperty(time);
 		this.id = new SimpleStringProperty(id);
 		this.length = new SimpleStringProperty(length);
 		this.data = new SimpleStringProperty(data);
+		this.ascii = new SimpleStringProperty(ascii);
 	}
 	
 	/**
-	 * @return
+	 * Public method to set the ASCII value.
+	 * 
+	 * @param ascii ASCII value of data field.
+	 * 
 	 */
-	public String getTime() {
+	public void setAscii(String ascii) {
+		this.ascii.set(ascii);
+	}
+	
+
+	/**
+	 * Returns ascii value of the current frame.
+	 * 
+	 * @return ASCII value of current frame.
+	 * 
+	 */
+	public String getAscii() {
+		return ascii.get();
+	}
+	
+	/**
+	 * Returns the time of the current frame.
+	 * 
+	 * @return Time of the current frame.
+	 * 
+	 */
+	public long getTime() {
 		return time.get();
 	}
 	
 	/**
-	 * @return
+	 * Returns the identifier of the current frame.
+	 * 
+	 * @return Identifier of the current frame.
+	 * 
 	 */
 	public String getId() {
 		return id.get();
 	}
 	
 	/**
-	 * @return
+	 * Returns the length of the data field.
+	 * 
+	 * @return Length of the data field.
+	 * 
 	 */
 	public String getLength() {
 		return length.get();
 	}
 	
 	/**
-	 * @return
+	 * 
+	 * Returns the data of the current frame.
+	 * 
+	 * @return Data of the current frame.
+	 * 
 	 */
 	public String getData() {
 		return data.get();
 	}
 	
 	/**
-	 * @param time
+	 * Sets the time of the frame.
+	 * 
+	 * @param time Time of the frame.
+	 * 
 	 */
-	public void setTime(String time) {
+	public void setTime(long time) {
 		this.time.set(time);
 	}
 	
 	/**
-	 * @param id
+	 * Sets the ID of the frame.
+	 * 
+	 * @param id Identifier of the frame.
+	 * 
 	 */
 	public void setId(String id) {
 		this.id.set(id);
 	}
 	
 	/**
-	 * @param length
+	 * Sets the length of the data field.
+	 * 
+	 * @param length Length of the data field.
+	 * 
 	 */
 	public void setLength(String length) {
 		this.length.set(length);
 	}
 	
 	/**
-	 * @param data
+	 * Sets the data o the frame.
+	 * 
+	 * @param data Data of the frame.
+	 * 
 	 */
 	public void setData(String data) {
 		this.data.set(data);
 	}
-	
-	
 	
 }
