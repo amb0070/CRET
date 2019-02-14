6b5ff1e3297910d2981bf340573e0ec3022bd81c
Adrian Marcos <37064592+amb0070@users.noreply.github.com>
2019-02-14 02:23:40 +0100

    Updated comments
    Updated comments from classes.

--- src/com/cret/db/DbUtils.java
+++ src/com/cret/db/DbUtils.java
@@ -9,7 +9,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-
 /**
  * 
  * Database utilities.
@@ -22,7 +21,6 @@ import java.sql.Statement;
  */
 public class DbUtils {
 
-	
 	/**
 	 * Connects to database and returns the connection.
 	 * 
@@ -30,18 +28,17 @@ public class DbUtils {
 	 */
 	public static Connection connect() {
 
-	        String url = "jdbc:sqlite:cret.db";
-	        
-	        Connection conn = null;
-	        
-	        try {
-	            conn = DriverManager.getConnection(url);
-	        } catch (SQLException e) {
-	        	GuiUtils.generateAlert(AlertType.ERROR, "INTERNAL ERROR", "Error connecting to database.");
-	        }
-	        return conn;
-	    }
+		String url = "jdbc:sqlite:cret.db";
 
+		Connection conn = null;
+
+		try {
+			conn = DriverManager.getConnection(url);
+		} catch (SQLException e) {
+			GuiUtils.generateAlert(AlertType.ERROR, "INTERNAL ERROR", "Error connecting to database.");
+		}
+		return conn;
+	}
 
 	/**
 	 * 
@@ -53,70 +50,66 @@ public class DbUtils {
 	public static boolean checkDatabaseExist(String dbName) {
 
 		File f = new File(dbName);
-		
+
 		/**
 		 * Check if file exists.
 		 */
 		if (!f.exists()) {
 			return false;
 		}
-		
+
 		String url = "jdbc:sqlite:" + dbName;
-		
+
 		String table1 = "SELECT name FROM sqlite_master WHERE type='table' AND name='data'";
-		
+
 		String table2 = "SELECT name FROM sqlite_master WHERE type='table' AND name='projects'";
-		
+
 		/**
 		 * Checks if it's possible to connect and checks the structure.
 		 */
-		
-		try (Connection con = DriverManager.getConnection(url)){
-			
+
+		try (Connection con = DriverManager.getConnection(url)) {
+
 			if (con != null) {
 
-				
-                Statement stmt = con.createStatement();
-                
-                ResultSet rs = stmt.executeQuery(table1);
-                
-                if (!rs.next()) {
-                	return false;
-                }
-                while(rs.next()) {
-                	System.out.println(rs.getString("name"));
-                	if(!rs.getString("name").equals("data")) {
-                		return false;
-                	}
-                }
-                
-                rs = stmt.executeQuery(table2);
-                
-                if (!rs.next()) {
-                	return false;
-                }
-                
-                while(rs.next()) {
-                	System.out.println(rs.getString("name"));
-                	if (!rs.getString("name").equals("projects")) {
-                		return false;
-                	}
-                }
-                
-                return true;
-                
+				Statement stmt = con.createStatement();
+
+				ResultSet rs = stmt.executeQuery(table1);
+
+				if (!rs.next()) {
+					return false;
+				}
+				while (rs.next()) {
+					if (!rs.getString("name").equals("data")) {
+						return false;
+					}
+				}
+
+				rs = stmt.executeQuery(table2);
+
+				if (!rs.next()) {
+					return false;
+				}
+
+				while (rs.next()) {
+					if (!rs.getString("name").equals("projects")) {
+						return false;
+					}
+				}
+
+				return true;
+
 			} else {
-				
+
 				return false;
-				
+
 			}
-			
+
 		} catch (SQLException e) {
-			System.out.println(e.getMessage());
 			GuiUtils.generateAlert(AlertType.ERROR, "INTERNAL ERROR", "Error checking database");
 			return false;
 		}
-		
+
 	}
-	
+
 }
--- src/com/cret/db/database.java
+++ src/com/cret/db/database.java
@@ -10,7 +10,7 @@ import com.cret.gui.GuiUtils;
 import javafx.scene.control.Alert.AlertType;
 
 /**
- * 
+ * Database class. Creates the connection to SQLite database.
  * 
  * @author Adrian Marcos
  * @version 1.0
@@ -42,6 +42,7 @@ public class Database {
 
 	/**
 	 * Returns database name.
+	 * 
 	 * @return Database name.
 	 */
 	public String getDatabaseName() {
@@ -71,7 +72,7 @@ public class Database {
 
 	/**
 	 * 
-	 * @return
+	 * @return Database connection.
 	 * @throws SQLException
 	 */
 	public Connection connect() throws SQLException {
@@ -81,6 +82,13 @@ public class Database {
 		return conn;
 	}
 
+	/**
+	 * Method to make SQL query with parameters.
+	 * 
+	 * @param query SQL query.
+	 * @param param SQL parameters.
+	 * @return ResultSet with retrieved data.
+	 */
 	public ResultSet query(String query, String param) {
 
 		try {
@@ -93,11 +101,17 @@ public class Database {
 		}
 		return null;
 	}
-	
+
+	/**
+	 * Method to make SQL query without parameters.
+	 * 
+	 * @param query SQL query.
+	 * @return REsultSet with retrieved data.
+	 */
 	public ResultSet query(String query) {
 
 		try {
-			Statement stmt  = conn.createStatement();
+			Statement stmt = conn.createStatement();
 			rs = stmt.executeQuery(query);
 			return rs;
 		} catch (SQLException e) {
@@ -105,17 +119,30 @@ public class Database {
 		}
 		return null;
 	}
-	
+
+	/**
+	 * Method to delete infromation from database.
+	 * 
+	 * @param query SQL query.
+	 * @param value Value to delete.
+	 * @return True if deletion is ok.
+	 * @throws SQLException Query error.
+	 */
 	public boolean delete(String query, String value) throws SQLException {
 		PreparedStatement pstmt = conn.prepareStatement(query);
 		pstmt.setString(1, value);
-		if (pstmt.executeUpdate()>=0) {
+		if (pstmt.executeUpdate() >= 0) {
 			return true;
 		} else {
 			return false;
 		}
 	}
 
+	/**
+	 * Creates database if it does not exist.
+	 * 
+	 * @param dbName Database name
+	 */
 	public void createTables(String dbName) {
 
 		/**
@@ -127,10 +154,10 @@ public class Database {
 		 * );
 		 */
 
-		String tableProjects = "CREATE TABLE projects (\n" + "name varchar(30) NOT NULL,\n" + "PRIMARY KEY (name)\n"
-				+ ");\r\n";
+		final String tableProjects = "CREATE TABLE projects (\n" + "name varchar(30) NOT NULL,\n"
+				+ "PRIMARY KEY (name)\n" + ");\r\n";
 
-		String tableData = "CREATE TABLE data (" + "name varchar(20) NOT NULL," + "ID varchar(10) NOT NULL,"
+		final String tableData = "CREATE TABLE data (" + "name varchar(20) NOT NULL," + "ID varchar(10) NOT NULL,"
 				+ "byte varchar(20) NOT NULL," + "projectName varchar(30) NOT NULL,"
 				+ "PRIMARY KEY (name, ID, byte, projectName)," + "FOREIGN KEY (projectName) REFERENCES projects(name)"
 				+ ");";
@@ -141,9 +168,7 @@ public class Database {
 			GuiUtils.generateAlert(AlertType.INFORMATION, "Success", "Database has been created!");
 
 		} catch (SQLException e) {
-			System.out.println(e.getMessage());
 			GuiUtils.generateAlert(AlertType.ERROR, "INTERNAL ERROR", "Error creating database");
 		}
-
 	}
 }
