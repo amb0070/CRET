ac149a0fca2d2a936f42b610f933b1a4a5f83531
Adrian Marcos <37064592+amb0070@users.noreply.github.com>
2019-02-14 02:30:55 +0100

    Update of comments
    Commented classes. Deleted old class.

--- src/com/cret/projects/json.java
+++ src/com/cret/projects/json.java
@@ -2,18 +2,41 @@ package com.cret.projects;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.util.List;
-import java.util.Map;
-import org.json.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import com.cret.db.Database;
+import com.cret.db.DbUtils;
+import com.cret.gui.GuiUtils;
+import javafx.scene.control.Alert.AlertType;
 
+/**
+ * 
+ * Class to generate JSON files and parse it.
+ * 
+ * Imports and exports files into internal database.
+ * 
+ * @author Adrian Marcos
+ * @version 1.0
+ *
+ */
 public class json {
-	
+
+	/**
+	 * Read file and makes it a String.
+	 * 
+	 * @param file JSON file.
+	 * @return Content with file content.
+	 * @throws IOException File not found.
+	 */
 	public static String fileToString(File file) throws IOException {
-		
+
 		BufferedReader reader = new BufferedReader(new FileReader(file));
 		StringBuilder stringBuilder = new StringBuilder();
 		String line = null;
@@ -22,60 +45,140 @@ public class json {
 			stringBuilder.append(line);
 			stringBuilder.append(ls);
 		}
-		// delete the last new line separator
 		stringBuilder.deleteCharAt(stringBuilder.length() - 1);
 		reader.close();
 
 		String content = stringBuilder.toString();
-		
+
 		return content;
 	}
 
-	
-	public static boolean isTextFile(File f) throws IOException {
-        String type = Files.probeContentType(f.toPath());
-        if (type == null) {
-            //type couldn't be determined, assume binary
-            return false;
-        } else if (type.startsWith("text")) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-	
-	public static boolean isJSON(String test) {
-		
-	    try {
-	    	
-	        new JSONObject(test);
-	        
-	    } catch (JSONException ex) {
-	        try {
-	            new JSONArray(test);
-	        } catch (JSONException ex1) {
-	            return false;
-	        }
-	    }
-	    return true;
-	}
-	
-	
-	public static void exportJSONProject(String name, Map<String, List<String>> values) {
-		
-		//Get data, generate json and open window dialog to store it.
-		
-	}
-	
-	public static void exportJSONProjects(String db) {
-		//Export all database
-	}
-	
-	public static void importJSONProjects(String file) {
-		
-		//Open window, select file, open, parse it and store in project options.
+	/**
+	 * Checks if file is a JSON.
+	 * 
+	 * @param fileToCheck File to check.
+	 * @return True if it is JSON content.
+	 */
+	public static boolean isJSON(String fileToCheck) {
 
-		
+		try {
+
+			new JSONObject(fileToCheck);
+
+		} catch (JSONException ex) {
+			try {
+				new JSONArray(fileToCheck);
+			} catch (JSONException ex1) {
+				return false;
+			}
+		}
+		return true;
 	}
-	
+
+	/**
+	 * Exports a selected project into JSON.
+	 * 
+	 * @param projectName Project name to export.
+	 * @return JSONObject JSONObject with the content.
+	 * @throws IOException
+	 * @throws JSONException
+	 * @throws SQLException
+	 */
+	public static JSONObject exportJSONProject(String projectName) throws IOException, JSONException, SQLException {
+
+		JSONObject completeJson = new JSONObject();
+
+		JSONObject elementJson = null;
+
+		Database db = new Database("cret.db");
+
+		String sql = "SELECT name, ID, byte FROM data WHERE projectName = ?";
+
+		ResultSet rs = db.query(sql, projectName);
+
+		while (rs.next()) {
+
+			elementJson = new JSONObject();
+
+			String dataName = rs.getString("name");
+			String id = rs.getString("ID");
+			String byteNumber = rs.getString("byte");
+
+			elementJson.put("Data", dataName).put("ID", id).put("Byte", byteNumber);
+
+			completeJson.append("values", elementJson);
+
+		}
+		completeJson.put("projectName", projectName);
+		db.disconnect();
+		return completeJson;
+
+	}
+
+	/**
+	 * Imports JSON file to database.
+	 * 
+	 * @param strFile JSON file.
+	 * @throws SQLException Query error.
+	 */
+	public static void importJSONProject(String strFile) throws SQLException {
+
+		JSONObject jsonFile = new JSONObject(strFile);
+
+		String sql = "INSERT INTO projects VALUES (?)";
+		String sql2 = "INSERT INTO data VALUES (?,?,?,?)";
+
+		String querySelect = "SELECT name FROM projects WHERE name = ?";
+
+		boolean duplicated = false;
+
+		if (jsonFile.has("projectName")) {
+			String projectName = jsonFile.getString("projectName");
+
+			try (Connection conn = DbUtils.connect();
+
+					PreparedStatement pstmtQuery = conn.prepareStatement(querySelect)) {
+				pstmtQuery.setString(1, projectName);
+				ResultSet rs = pstmtQuery.executeQuery();
+				if (rs.next()) {
+					duplicated = true;
+					GuiUtils.generateAlert(AlertType.INFORMATION, "Duplicated project.",
+							"Project name already exists. Please, select another name.");
+				}
+			}
+
+			if (!duplicated) {
+				try (Connection conn = DbUtils.connect();
+
+						PreparedStatement pstmt = conn.prepareStatement(sql)) {
+					pstmt.setString(1, projectName);
+					pstmt.executeUpdate();
+
+					JSONArray array = jsonFile.getJSONArray("values");
+					for (int i = 0; i < array.length(); i++) {
+						String id = array.getJSONObject(i).getString("ID");
+						String name = array.getJSONObject(i).getString("Data");
+						String byteNumber = array.getJSONObject(i).getString("Byte");
+
+						PreparedStatement pstmt2 = conn.prepareStatement(sql2);
+						pstmt2.setString(1, name);
+						pstmt2.setString(2, id);
+						pstmt2.setString(3, byteNumber);
+						pstmt2.setString(4, projectName);
+						pstmt2.executeUpdate();
+
+					}
+
+					GuiUtils.generateAlert(AlertType.INFORMATION, "Success", "Project has been imported.");
+
+				}
+
+			}
+
+		} else {
+			GuiUtils.generateAlert(AlertType.INFORMATION, "Error", "Not valid JSON file");
+		}
+
+	}
+
 }
--- src/com/cret/projects/project.java
+++ src/com/cret/projects/project.java
@@ -1,188 +1,139 @@
 package com.cret.projects;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
+import com.cret.db.DbUtils;
 import com.cret.gui.GuiUtils;
-
 import javafx.scene.control.Alert.AlertType;
 
+/**
+ * 
+ * Class to manage projects.
+ * 
+ * @author Adrian Marcos
+ * @version 1.0
+ *
+ */
 public class Project {
-	
+
+	/**
+	 * Constructror. Sets the project name.
+	 * @param name Project name.
+	 */
 	public Project(String name) {
-		
 		setProjectName(name);
-		
 	}
-	
-	//PROJECT
-		//Name
-		//MAP
-			//Data
-			//ID
-			//Byte
-	
-	//Project Name
+
+
 	private String projectName;
-	
-	//Map with analyzed can data
-	//Structure:
-		//Data, ID, Byte
-			//List 0 - ID
-			//List 1 - Byte
-	
+
+	/**
+	 * Map to store identified values.
+	 */
 	private Map<String, List<String>> analyzedValues = new HashMap<>();
+
 	
-	
-	private Connection connect() {
-		String url = "jdbc:sqlite:cret.db";
-		Connection conn = null;
-		
-		try {
-			conn = DriverManager.getConnection(url);
-		} catch (SQLException e) {
-			System.out.println(e.getMessage());
-		}
-		return conn;
-	}
-	
-	//DONE
+	/**
+	 * Sets the project name.
+	 * @param name New project name.
+	 */
 	public void setProjectName(String name) {
 		projectName = name;
 	}
+
 	
-	//DONE
+	/**
+	 * Returns the project name.
+	 * @return String String with project name.
+	 */
 	public String getProjetName() {
 		return this.projectName;
 	}
+
 	
-	//DONE
-	public Map<String, List<String>> getAllData(){
+	/**
+	 * Retruns all analyzed values.
+	 * 
+	 * @return Map Map with analyzed values.
+	 */
+	public Map<String, List<String>> getAllData() {
 		return analyzedValues;
 	}
-	
-	//TO TEST
-	//Return map value 1 from key (ID)
-	public String getMapValue1(String key) {
-		
-		if (analyzedValues.containsKey(key)) {
-			
-			List<String> values = analyzedValues.get(key);
-			
-			return values.get(0);
-			
-		} else {
-			return "";
-		}
-	}
-	
-	//TO TEST
-	//Return map value 2 from key (byte)
-	public String getMapValue2(String key) {
-		
-		if (analyzedValues.containsKey(key)) {
-			
-			List<String> values = analyzedValues.get(key);
-			
-			return values.get(1);
-			
-		} else {
-			return "";
-		}
-	}
-	
-	//TO TEST
-	//Return all keys (identified names)
+
+	/**
+	 * Returns keys of the map.
+	 * @return List List with keys of the map.
+	 */
 	public List<String> getMapKeys() {
-		
+
 		List<String> list = new ArrayList<String>(analyzedValues.keySet());
-		
+
 		return list;
 	}
+
 	
-	//TO TEST
+	/**
+	 * Sets the map.
+	 * @param newMap New map to set.
+	 */
 	public void setCompleteMap(Map<String, List<String>> newMap) {
 		analyzedValues = newMap;
 	}
+
 	
-	
-	//TO TEST
+	/**
+	 * Creates a new key in the map.
+	 * @param key String with the new key.
+	 */
 	public void setMapKey(String key) {
-		
+
 		if (analyzedValues.containsKey(key)) {
-			GuiUtils.generateAlert(AlertType.ERROR, "ERROR EN MAP", "key repetidas bro");
+			GuiUtils.generateAlert(AlertType.ERROR, "INTERNAL ERROR", "Duplicated value");
 		} else {
-			
+
 			List<String> list = new ArrayList<String>();
-			//Add empty list
+			// Add empty list
 			analyzedValues.put(key, list);
 		}
 
-		
 	}
+
 	
-	//TO TEST
-	public void setMapValues(String key, String id, String byteData) {
-		
-		if (analyzedValues.containsKey(key)) {
-			
-			List<String> list = new ArrayList<String>();
-			list.add(id);
-			list.add(byteData);
-			
-			analyzedValues.put(key, list);
-			
-		} else {
-			
-			System.out.println("No key on map");
-		}
-		
-	}
-	
-	
-	//Save project in database
+	/**
+	 * Stores project in database.
+	 */
 	public void saveProject() {
-		
+
 		String sqlProject = "INSERT INTO projects VALUES(?)";
 		String sqlData = "INSERT INTO data VALUES (?,?,?,?)";
 		
-	       try (Connection conn = this.connect();
-	            PreparedStatement pstmt = conn.prepareStatement(sqlProject)) {
-	            pstmt.setString(1, this.projectName);
-	            pstmt.executeUpdate();
-	            PreparedStatement pstmt2 = conn.prepareStatement(sqlData);
-	            pstmt2 = conn.prepareStatement(sqlData);
-	            
-	    		for (String key : analyzedValues.keySet()) {
-	    			String id = analyzedValues.get(key).get(0);
-	    			String byteNumber = analyzedValues.get(key).get(1);
-	    			
-	    			pstmt2.setString(1, key);
-	    			pstmt2.setString(2, id);
-	    			pstmt2.setString(3, byteNumber);
-	    			pstmt2.setString(4, this.projectName);
-	    			pstmt2.executeUpdate();
-	    		}
-	            
-	        } catch (SQLException e) {
-	            System.out.println(e.getMessage());
-	        }
-	       
-	       
-		
-		
+		try (Connection conn = DbUtils.connect();
+			
+			PreparedStatement pstmt = conn.prepareStatement(sqlProject)) {
+			pstmt.setString(1, this.projectName);
+			pstmt.executeUpdate();
+			PreparedStatement pstmt2 = conn.prepareStatement(sqlData);
+			pstmt2 = conn.prepareStatement(sqlData);
 
-		
-		
-		
-		
-		
+			for (String key : analyzedValues.keySet()) {
+				String id = analyzedValues.get(key).get(0);
+				String byteNumber = analyzedValues.get(key).get(1);
+
+				pstmt2.setString(1, key);
+				pstmt2.setString(2, id);
+				pstmt2.setString(3, byteNumber);
+				pstmt2.setString(4, this.projectName);
+				pstmt2.executeUpdate();
+				
+			}
+
+		} catch (SQLException e) {
+			GuiUtils.generateAlert(AlertType.ERROR, "INTERNAL ERROR", "Internal SQL error.");
+		}
 	}
-
 }
